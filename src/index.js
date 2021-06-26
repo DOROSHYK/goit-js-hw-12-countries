99331abb776053ff493134433111d997cfd507b3
Doroshuk <dorosukolga100@gmail.com>
2021-06-26 10:42:59 +0300

    add hbs file

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 import './sass/main.scss';
 import Error from './js/pnotify.js';
+import fetchCountries from './js/fetchCountries';
+import  debounce  from 'lodash.debounce';
+import countryCard from './templates/oneCountry.hbs';
+import countriesList from './templates/countriesList.hbs';
